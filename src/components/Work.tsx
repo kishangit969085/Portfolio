a66cdb3241fe3068cb25331d944c865f4fa4f2fa
Kishan prajapati <kishangit969085@gmail.com>
2026-05-08 02:29:21 +0530

    fix deploy errors: add gsap-trial type, safe SplitText access, ignore tsbuildinfo

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,5 +1,4 @@
 import "./styles/Work.css";
-import WorkImage from "./WorkImage";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
